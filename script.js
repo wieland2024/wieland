b6b3f68c823433f82af1b78e4efec3c25cbf02a9
wieland2024 <xseryjx2020@outlook.de>
2024-08-02 21:05:08 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const users = {
-  "111": "222",
+  "log1": "pas1",
   "user2": "password2",
   "user3": "password3",
   "user4": "password4",
@@ -20,41 +20,9 @@ document.getElementById('loginForm').addEventListener('submit', function(event) 
 
   if (users[login] === password) {
     console.log('Правильный логин и пароль');
-    window.location.href = 'indexhome.html';
+    window.location.href = 'https://sites.google.com/view/wielandtest';
   } else {
     console.log('Неправильный логин или пароль');
     error.style.display = 'block';
   }
 });
-const menuToggle = document.querySelector('.menu-toggle');
-const menuContainer = document.querySelector('.menu-container');
-
-menuToggle.addEventListener('click', () => {
-  menuContainer.classList.toggle('open');
-});
-// Массив с вашими сообщениями
-const messages = [
-  "Сообщение 1: Добро пожаловать на наш сайт!",
-  "Сообщение 2: У нас новые акции и предложения!",
-  "Сообщение 3: Подписывайтесь на нашу рассылку!",
-  "Сообщение 4: Следите за обновлениями!",
-];
-
-// Функция для обновления бегущей строки
-function updateTicker() {
-  const ticker = document.getElementById("news-ticker");
-  let index = 0;
-
-  // Функция для отображения сообщений
-  function displayMessage() {
-    ticker.textContent = messages[index];
-    index = (index + 1) % messages.length; // Циклически переключаемся по массиву
-  }
-
-  // Вызываем displayMessage каждые 5 секунд
-  displayMessage(); // Показать первое сообщение сразу
-  setInterval(displayMessage, 5000); // Каждые 5000 миллисекунд (5 секунд)
-}
-
-// Запускаем функцию обновления бегущей строки после загрузки страницы
-window.onload = updateTicker;
